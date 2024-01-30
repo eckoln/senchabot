d70f9debc7f6648e6ccb1042b238fb37d4339622
eckoln <ahmettongul01@gmail.com>
2024-01-30 18:34:43 +0300

    feat(apps/web): create next-app

---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans]
+      }
+    }
+  },
+  plugins: []
+};
+
+export default config;
