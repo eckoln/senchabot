f1ec82d295d11d72eb15bce39a0e0b7810d04f2e
eckoln <ahmettongul01@gmail.com>
2024-02-25 12:33:01 +0300

    feat(apps/web): add 'marketing' route group

--- apps/web/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-2xl font-semibold underline">hello world</h1>
-    </main>
-  );
-}
