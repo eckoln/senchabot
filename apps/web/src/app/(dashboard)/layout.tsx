f37d56617a143631bbbb679e7ac23109b1cfac04
eckoln <ahmettongul01@gmail.com>
2024-02-25 15:15:57 +0300

    feat(apps/web): add 'dashboard' route group & layout

---
+++ apps/web/src/app/(dashboard)/layout.tsx
@@ -0,0 +1,18 @@
+import { Footer } from "@/components/footer";
+import { Sidebar } from "@/components/sidebar/sidebar";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="relative flex flex-1 flex-row">
+      <Sidebar />
+      <div className="ml-80 flex flex-grow flex-col overflow-x-hidden border-l">
+        <main className="flex-grow px-12 py-16">{children}</main>
+        <Footer />
+      </div>
+    </div>
+  );
+}
