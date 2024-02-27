066c0af935d222f0d3b9c028213fba5ed91a1449
Ahmet <ahmettongul01@gmail.com>
2024-02-27 19:23:46 +0300

    feat(apps/web): add ui/link component

---
+++ apps/web/src/components/ui/link.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+import NextLink, { type LinkProps as NextLinkProps } from "next/link";
+
+import { cn } from "@/lib/utils";
+
+export function Link({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"a"> & NextLinkProps) {
+  return (
+    <NextLink
+      className={cn(
+        "underline underline-offset-4 hover:text-primary",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
