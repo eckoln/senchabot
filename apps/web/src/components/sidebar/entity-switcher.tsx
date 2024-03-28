51730d24a241a196cf4bf3c486361bd215398477
Ahmet <ahmettongul01@gmail.com>
2024-03-28 11:50:00 +0300

    refactor(apps/web): entity-switcher-client moved as entity-switcher

--- apps/web/src/components/sidebar/entity-switcher.tsx
+++ apps/web/src/components/sidebar/entity-switcher.tsx
@@ -1,13 +1,71 @@
-import { EntitySwitcherClient } from "./entity-switcher-client";
+"use client";
 
-export async function EntitySwitcher() {
-  // transform to api call
-  let entities = [
-    //{ id: "twitch-1", label: "twitch-1", platform: "twitch" },
-    { id: "discord-1", label: "discord-1", platform: "discord" },
-    { id: "discord-2", label: "discord-2", platform: "discord" },
-    { id: "discord-3", label: "discord-3", platform: "discord" },
-  ];
+import { useMemo } from "react";
 
-  return <EntitySwitcherClient entities={entities} />;
+import {
+  useParams,
+  useRouter,
+  useSelectedLayoutSegment,
+} from "next/navigation";
+
+import { GetUserEntities } from "@/types";
+
+import { DiscordIcon, TwitchIcon } from "@/components/icons";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/ui/select";
+
+interface Props {
+  entities: GetUserEntities[];
+}
+
+export function EntitySwitcher({ entities }: Props) {
+  let currentPlatform = useSelectedLayoutSegment();
+  let params = useParams<{ id: string }>();
+  let router = useRouter();
+
+  let currentEntity = useMemo(() => {
+    if (currentPlatform !== "twitch" && currentPlatform !== "discord") {
+      return undefined;
+    }
+    return params.id;
+  }, [currentPlatform, params]);
+
+  function pushHandler(value: string) {
+    let foundEntity = entities.find((i) => value === i.platform_entity_id);
+    if (foundEntity) {
+      return router.push(`/dashboard/${foundEntity.platform}/${value}`);
+    }
+  }
+
+  return (
+    <Select
+      defaultValue={currentEntity}
+      onValueChange={pushHandler}
+      key={currentEntity}
+    >
+      <SelectTrigger>
+        <SelectValue placeholder="Select a Entity" />
+      </SelectTrigger>
+      <SelectContent>
+        {entities?.map((item, index) => (
+          <SelectItem value={item.platform_entity_id} key={index}>
+            <div className="flex items-center space-x-2">
+              {item.platform === "twitch" ? (
+                <TwitchIcon className="size-4" />
+              ) : (
+                <DiscordIcon className="size-4" />
+              )}
+              <span className="max-w-48 truncate">{item.entity_name}</span>
+            </div>
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
 }
