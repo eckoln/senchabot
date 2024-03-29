6972549380da9aa23eee311322bb1777701390f2
Ahmet <ahmettongul01@gmail.com>
2024-03-29 11:07:56 +0300

    feat(apps/web): add react-hot-toast

--- apps/web/src/app/(dashboard)/dashboard/layout.tsx
+++ apps/web/src/app/(dashboard)/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import { Toaster } from "react-hot-toast";
+
 import { Footer } from "@/components/footer";
 import { Sidebar } from "@/components/sidebar/sidebar";
 
@@ -7,12 +9,15 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="relative flex flex-1 flex-row">
-      <Sidebar />
-      <div className="ml-80 flex flex-grow flex-col overflow-x-hidden border-l">
-        <main className="flex-grow px-12 py-16">{children}</main>
-        <Footer />
+    <>
+      <div className="relative flex flex-1 flex-row">
+        <Sidebar />
+        <div className="ml-80 flex flex-grow flex-col overflow-x-hidden border-l">
+          <main className="flex-grow px-12 py-16">{children}</main>
+          <Footer />
+        </div>
       </div>
-    </div>
+      <Toaster />
+    </>
   );
 }
