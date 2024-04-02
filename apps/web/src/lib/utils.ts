fd3c9d758dd6e64c587de529263dfb369c1b844f
Ahmet <ahmettongul01@gmail.com>
2024-04-02 16:05:00 +0300

    feat(apps/web): add toDate hook

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function toDate(date: Date) {
+  return new Intl.DateTimeFormat("en").format(date);
+}
