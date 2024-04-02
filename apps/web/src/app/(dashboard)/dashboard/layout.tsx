888f041b4c40c569670611497a96cb3d54849be0
Ahmet <ahmettongul01@gmail.com>
2024-04-02 10:41:31 +0300

    style(apps/web): fix border

--- apps/web/src/app/(dashboard)/dashboard/layout.tsx
+++ apps/web/src/app/(dashboard)/dashboard/layout.tsx
@@ -13,7 +13,7 @@ export default function Layout({
     <>
       <div className="relative flex flex-1 flex-row">
         <Sidebar />
-        <div className="ml-80 flex flex-grow flex-col overflow-x-hidden border-l">
+        <div className="ml-80 flex flex-grow flex-col overflow-x-hidden">
           <main className="flex-grow px-12 py-16">{children}</main>
           <Footer />
         </div>
