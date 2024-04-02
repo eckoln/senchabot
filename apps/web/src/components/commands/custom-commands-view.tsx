d07c4aa2432b1cfff56ea843859e511928a88b03
Ahmet <ahmettongul01@gmail.com>
2024-04-02 17:55:19 +0300

    feat(apps/web): add reusable `custom` and `global` commands page views

---
+++ apps/web/src/components/commands/custom-commands-view.tsx
@@ -0,0 +1,82 @@
+import type { EntityCommands, Platforms } from "@/types";
+import { Share1Icon } from "@radix-ui/react-icons";
+
+import { Button } from "@/ui/button";
+import { Switch } from "@/ui/switch";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/ui/table";
+
+import { CreateButton } from "./create-button";
+import { DeleteButton } from "./delete-button";
+import { UpdateButton } from "./update-button";
+
+interface Props {
+  platform: Platforms;
+  commands: EntityCommands[];
+}
+
+export function CustomCommandsView({ platform, commands }: Props) {
+  return (
+    <div className="space-y-4">
+      <div className="flex h-9 items-center justify-between">
+        <p className="text-sm text-muted-foreground">
+          {commands?.length} items found.
+        </p>
+        <div className="flex flex-row space-x-3">
+          <ShareButton />
+          <CreateButton platform={platform} />
+        </div>
+      </div>
+      <div className="relative w-full overflow-auto rounded-md border bg-card">
+        <Table className="table-fixed">
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-20">Status</TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead>Content</TableHead>
+              <TableHead />
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {commands?.map((item) => (
+              <TableRow key={item.id}>
+                <TableCell>
+                  <div className="flex items-center">
+                    <Switch defaultChecked={item.status} />
+                  </div>
+                </TableCell>
+                <TableCell>!{item.name}</TableCell>
+                <TableCell>
+                  <p className="truncate" title={item.content}>
+                    {item.content}
+                  </p>
+                </TableCell>
+                <TableCell>
+                  <div className="flex justify-end space-x-1.5">
+                    <UpdateButton command={item} />
+                    <DeleteButton command={item} />
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+}
+
+function ShareButton() {
+  return (
+    <Button variant="secondary">
+      <Share1Icon className="size-4" />
+      <span>Share</span>
+    </Button>
+  );
+}
---
+++ apps/web/src/components/commands/global-commands-view.tsx
@@ -0,0 +1,48 @@
+import type { EntityCommands } from "@/types";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/ui/table";
+
+interface Props {
+  commands: EntityCommands[];
+}
+
+export default async function GlobalCommandsView({ commands }: Props) {
+  return (
+    <div className="space-y-4">
+      <div className="flex h-9 items-center justify-between">
+        <p className="text-sm text-muted-foreground">
+          {commands?.length} items found.
+        </p>
+      </div>
+      <div className="relative w-full overflow-auto rounded-md border bg-card">
+        <Table className="table-fixed">
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Content</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {commands?.map((item, index) => (
+              <TableRow key={index}>
+                <TableCell>!{item.name}</TableCell>
+                <TableCell>
+                  <p className="truncate" title={item.content}>
+                    {item.content}
+                  </p>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+}
