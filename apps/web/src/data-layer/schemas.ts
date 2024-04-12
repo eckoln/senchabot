2fe79f3ed5a33562056c66fd343364c53d7b44c6
eckoln <ahmettongul01@gmail.com>
2024-04-12 17:05:04 +0300

    feat(apps/web): validation schema updates

--- apps/web/src/data-layer/schemas.ts
+++ apps/web/src/data-layer/schemas.ts
@@ -10,22 +10,21 @@ export const createCommandSchema = z.object({
   platformEntityId: z.string().min(1),
   command_name: z.string().min(1),
   command_content: z.string().min(1),
-  status: z.boolean(),
+  status: z.coerce.boolean(),
 });
 
 export const updateCommandSchema = z.object({
   platform: platforms,
   platformEntityId: z.string().min(1),
-  id: z.number().min(1),
-  command_name: z.string().min(1),
+  id: z.string().min(1),
   command_content: z.string().min(1),
-  status: z.boolean(),
+  status: z.coerce.boolean(),
 });
 
 export const deleteCommandSchema = z.object({
   platform: platforms,
   platformEntityId: z.string().min(1),
-  id: z.number().min(1),
+  id: z.string().min(1),
 });
 
 /*
@@ -35,7 +34,7 @@ export const createAnnouncementSchema = z.object({
   platformEntityId: z.string().min(1),
   twitch_username: z.string().min(1),
   guild_channel_id: z.string().min(1),
-  anno_content: z.string().optional(),
+  announcement_content: z.string().optional(),
 });
 
 export const deleteAnnouncementSchema = z.object({
@@ -58,5 +57,8 @@ export const deleteEventChannelSchema = z.object({
  */
 export type CreateCommandSchema = z.infer<typeof createCommandSchema>;
 export type UpdateCommandSchema = z.infer<typeof updateCommandSchema>;
+export type DeleteCommandSchema = z.infer<typeof deleteCommandSchema>;
 export type CreateAnnouncementSchema = z.infer<typeof createAnnouncementSchema>;
+export type DeleteAnnouncementSchema = z.infer<typeof deleteAnnouncementSchema>;
 export type CreateEventChannelSchema = z.infer<typeof createEventChannelSchema>;
+export type DeleteEventChannelSchema = z.infer<typeof deleteEventChannelSchema>;
