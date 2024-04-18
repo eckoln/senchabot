e3cec3b60824c1390ce8bc68247fdaac4bd6c50e
eckoln <ahmettongul01@gmail.com>
2024-04-18 19:34:42 +0300

    feat(apps/web): account/profile data

--- apps/web/src/app/(dashboard)/dashboard/account/@tabs/profile/page.tsx
+++ apps/web/src/app/(dashboard)/dashboard/account/@tabs/profile/page.tsx
@@ -1,16 +1,25 @@
+import { redirect } from 'next/navigation'
+
+import { auth } from '@/lib/auth'
 import { Input } from '@/ui/input'
 import { Label } from '@/ui/label'
 
-export default function Page() {
+export default async function Page() {
+  let session = await auth()
+
+  if (!session) {
+    redirect('/signin')
+  }
+
   return (
-    <div className="max-w-lg space-y-8">
+    <div className="max-w-md space-y-8">
       <div className="space-y-2">
         <Label>Name</Label>
-        <Input value="John Doe" disabled />
+        <Input value={session.user?.name!} disabled />
       </div>
       <div className="space-y-2">
         <Label>Email</Label>
-        <Input value="example@mail.com" disabled />
+        <Input value={session.user?.email!} disabled />
       </div>
     </div>
   )
