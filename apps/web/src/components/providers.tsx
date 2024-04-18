fe4b5b05f094cf3af30e188dd77fb3011e6de686
eckoln <ahmettongul01@gmail.com>
2024-04-18 18:34:05 +0300

    feat(apps/web): change next-themes default theme from dark to system

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -1,13 +1,13 @@
 'use client'
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { type ThemeProviderProps } from 'next-themes/dist/types'
+import type { ThemeProviderProps } from 'next-themes/dist/types'
 
 export function ThemeProvider(props: ThemeProviderProps) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="dark"
+      defaultTheme="system"
       enableSystem
       disableTransitionOnChange
       {...props}
